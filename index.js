ea1e71ad39e6883035c801cf1c38d65c64c7ca86
TomasBotello <tbotello73@gmail.com>
2023-08-29 14:54:16 -0600

    Creacion de bd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hola")
+const persona ={
+    nombre: "Sergio",
+    apellidoPaterno: "Valdez",
+    apellidoMaterno: "Rodela",
+    semestre:7
+}
+ const fruta=["naranja","manzana","platano","tomate",2,8,true,{
+        nombre: "Sergio",
+        apellidoPaterno: "Valdez",
+        apellidoMaterno: "Rodela",
+        semestre:7
+ }]
+
+ /*console.log(fruta[7].nombre)*/
+
+
+ function promedio(u1,u2,u3,u4){
+    let prom = (u1+u2+u3+u4)/4
+    return prom
+ }
+
+ console.log(promedio(8,9,7,9.5))
